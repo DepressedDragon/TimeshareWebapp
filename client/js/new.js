bf47ce85aa415213f5906e6f09ff9734cd5bf444
henryshughes <hockeyfan534@gmail.com>
2017-05-29 20:17:21 -0400

    Stripe start
    Still getting error message.

--- client/js/new.js
+++ client/js/new.js
@@ -2,7 +2,93 @@ Router.route('/calendar');
 Router.route('/home');
 Router.route('/booking');
 Router.route('/');
+Router.route('/payment')
+/*
+if (Meteor.isClient){
+	Meteor.startup(function(){
+		Stripe.setPublishableKey(Meteor.settings.public.StripePub);
+	})
 
+	Template.body.events({
+		submit . payment-form: function(event){
+			even.preventDefault();
+
+			var cardDetails = {
+				"number": event.target.cardNumber.value,
+				"cvc": event.target.cardCVC.value,
+				"exp_month": event.target.cardExpiry.MM.value,
+				"exp_year": event.target.cardExpiry.YY.value,
+			}
+			Stripe.createToken(cardDetails, function()status, result){
+				if(result.error){
+					alert(result.error.message);
+				}
+				else{
+					console.log(result)
+				}
+			})
+		}
+	})
+}
+if (Meteor.isServer){
+
+}
+
+*/
+if (Meteor.isClient) {
+  Meteor.startup(function(){
+    Stripe.setPublishableKey(Meteor.settings.public.StripePub);
+  });
+
+  Template.body.events({
+    "submit .payment-form": function(event){
+      event.preventDefault();
+
+      var cardDetails = {
+        "number": event.target.cardNumber.value,
+        "cvc": event.target.cardCVC.value,
+        "exp_month": event.target.cardExpiryMM.value,
+        "exp_year": event.target.cardExpiryYY.value
+      }
+
+      Stripe.createToken(cardDetails, function(status, result){
+        if(result.error){
+          alert(result.error.message);
+        }else{
+          Meteor.call("chargeCard", result.id, function(err, response){
+            if(err){
+              alert(err.message);
+            }else{
+              console.log(response);
+              alert("You were successfully charged:" + response);
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
+if (Meteor.isServer) {
+  var stripe = StripeAPI(Meteor.settings.StripePri);
+
+  Meteor.methods({
+    "chargeCard": function(cardToken){
+      stripe.charges.create({
+        amount: 500,
+        currency: "aud",
+        source: cardToken
+      }, function(err, result){
+        if(err){
+          throw new Meteor.error(500, "stripe-error", err.message);
+        }else{
+          console.log(result);
+          return result;
+        }
+      })
+    }
+  })
+}
 
 
 /*Template.userRegister.events({
