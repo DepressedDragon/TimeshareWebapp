3db82380108b6aacfbd1c5abc651edb8d4c6bf5d
henryshughes <hockeyfan534@gmail.com>
2017-05-31 12:40:37 -0400

    Working credit card payment
    Credit card payment with stripe tokens 100% working, called on by a
    button.  Has been tested.

--- client/js/new.js
+++ client/js/new.js
@@ -2,9 +2,48 @@ Router.route('/calendar');
 Router.route('/home');
 Router.route('/booking');
 Router.route('/');
-Router.route('/payment')
+Router.route('/payment');
+
+if (Meteor.isClient) {
+  Template.payment.events({
+    'click button': function(e) {
+      e.preventDefault();
+
+      StripeCheckout.open({
+        key: 'pk_test_dYxemQdfb6WJxKWHakXhYPBV',
+        amount: 5000, // this is equivalent to $50
+        name: 'Timeshare booking',
+        description: 'Three nights - $120',
+        panelLabel: 'Book now!',
+        token: function(res) {
+          stripeToken = res.id;
+          console.info(res);
+          Meteor.call('chargeCard', stripeToken);
+        }
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    'chargeCard': function(stripeToken) {
+      check(stripeToken, String);
+      var Stripe = StripeAPI('sk_test_y6fnJrdaQKd9MAolZcm3uLqI');
+
+      Stripe.charges.create({
+        source: stripeToken,
+        amount: 5000, // this is equivalent to $50
+        currency: 'cad'
+      }, function(err, charge) {
+        console.log(err, charge);
+      });
+    }
+  });
+}
 
 
+/*
 
 Template.payment.onCreated(() => {
   let template = Template.instance();
@@ -55,7 +94,7 @@ Template.payment.events({
 })
 
 
-/*
+
 
 if (Meteor.isClient){
 	Meteor.startup(function(){
