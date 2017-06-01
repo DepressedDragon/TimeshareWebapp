802973f8c74d240fa702c7c0d2604a72fad2781c
henryshughes <hockeyfan534@gmail.com>
2017-06-01 09:23:50 -0400

    Fixed errors

--- client/js/new.js
+++ client/js/new.js
@@ -25,20 +25,3 @@ if (Meteor.isClient) {
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.methods({
-    'chargeCard': function(stripeToken) {
-      check(stripeToken, String);
-      var Stripe = StripeAPI('sk_test_y6fnJrdaQKd9MAolZcm3uLqI');
-
-      Stripe.charges.create({
-        source: stripeToken,
-        amount: 50, // this is equivalent to $50
-        currency: 'cad'
-      }, function(err, charge) {
-        console.log(err, charge);
-      });
-    }
-  });
-}
-
